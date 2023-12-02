1a451f03c3f677dc224864a8e6ce3bb3d6607d23
Tom Willemsen <tomwillemsen1995@gmail.com>
2023-12-02 06:42:09 +0000

    Fix energy benchmarking setup for newer kernel

--- run_all_2023.sh
+++ run_all_2023.sh
@@ -14,9 +14,7 @@ do
         hyperfine $HYPERFINE_RUN_ARGS -N -u millisecond --style basic "$CMD" 2>/dev/null
         
         # CPU energy usage benchmark
-        CPU_JOULES_ITER=10
-        CPU_JOULES=$(perf stat -r $CPU_JOULES_ITER -e power/energy-pkg/ -- $CMD 2>&1 >/dev/null | grep -oE "[0-9\.]+ Joules power/energy-pkg/" | cut -d ' ' -f 1 )
-        CPU_JOULES_PER_RUN=$(echo "scale=4; $CPU_JOULES/$CPU_JOULES_ITER" | bc -l)
-        echo "CPU Joules per run: $CPU_JOULES_PER_RUN J"
+        CPU_JOULES_ITER=20
+        perf stat -r $CPU_JOULES_ITER -e power/energy-pkg/ -- $CMD 2>&1 >/dev/null | grep -F "Joules"
     fi
 done;

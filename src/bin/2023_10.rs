43179ade0010a9d2508b7181bb95597945d14882
Tom Willemsen <tomwillemsen1995@gmail.com>
2023-12-10 20:23:16 +0000

    10: handle start correctly

--- src/bin/2023_10.rs
+++ src/bin/2023_10.rs
@@ -1,8 +1,8 @@
 #![cfg_attr(feature = "bench", feature(test))]
 use advent_of_code_2023::{Cli, Parser};
+use ahash::AHashSet;
 use ndarray::Array2;
 use std::fs;
-use ahash::AHashSet;
 
 #[derive(Eq, PartialEq, Hash, Clone, Copy)]
 struct Point {
@@ -18,7 +18,10 @@ struct PointDiff {
 
 impl Point {
     fn apply_diff(&self, diff: &PointDiff) -> Option<Point> {
-        Some(Point { x: self.x.checked_add_signed(diff.x)?, y: self.y.checked_add_signed(diff.y)? })
+        Some(Point {
+            x: self.x.checked_add_signed(diff.x)?,
+            y: self.y.checked_add_signed(diff.y)?,
+        })
     }
 }
 
@@ -31,103 +34,109 @@ fn parse(raw_inp: &str) -> Array2<u8> {
 
     Array2::from_shape_vec(
         ((raw_inp.trim().len() + 1) / (columns + 1), columns),
-        raw_inp
-            .bytes()
-            .filter(|&x| x != b'\n')
-            .collect(),
+        raw_inp.bytes().filter(|&x| x != b'\n').collect(),
     )
     .expect("can't make array")
 }
 
 fn get_rules_for(itm: u8) -> Vec<PointDiff> {
-    if itm == b'|' {
-        vec![
-            PointDiff { y: -1, x: 0 },
-            PointDiff { y: 1, x: 0 },
-        ]
-    } else if itm == b'-' {
-        vec![
-            PointDiff { y: 0, x: 1 },
-            PointDiff { y: 0, x: -1 },
-        ]
-    } else if itm == b'L' {
-        vec![
-            PointDiff { y: -1, x: 0 },
-            PointDiff { y: 0, x: 1 },
-        ]
-    } else if itm == b'J' {
-        vec![
-            PointDiff { y: -1, x: 0 },
-            PointDiff { y: 0, x: -1 },
-        ]
-    } else if itm == b'7' {
-        vec![
-            PointDiff { y: 1, x: 0 },
-            PointDiff { y: 0, x: -1 },
-        ]
-    } else if itm == b'F' {
-        vec![
-            PointDiff { y: 1, x: 0 },
-            PointDiff { y: 0, x: 1 },
-        ]
-    } else if itm == b'S' {
-        vec![
-            // TODO HACK FIXME
-            // PointDiff { y: 1, x: 0 },
-            PointDiff { y: 0, x: 1 },
-            // PointDiff { y: -1, x: 0 },
-            // PointDiff { y: 0, x: -1 },
-        ]
-    } else {
-        vec![]
+    match itm {
+        b'|' => vec![PointDiff { y: -1, x: 0 }, PointDiff { y: 1, x: 0 }],
+        b'-' => vec![PointDiff { y: 0, x: 1 }, PointDiff { y: 0, x: -1 }],
+        b'L' => vec![PointDiff { y: -1, x: 0 }, PointDiff { y: 0, x: 1 }],
+        b'J' => vec![PointDiff { y: -1, x: 0 }, PointDiff { y: 0, x: -1 }],
+        b'7' => vec![PointDiff { y: 1, x: 0 }, PointDiff { y: 0, x: -1 }],
+        b'F' => vec![PointDiff { y: 1, x: 0 }, PointDiff { y: 0, x: 1 }],
+        _ => vec![],
     }
 }
 
-fn get_main_loop(data: &Array2<u8>) -> AHashSet<Point> {
+const LEFT: PointDiff = PointDiff { y: 0, x: -1 };
+const RIGHT: PointDiff = PointDiff { y: 0, x: 1 };
+const UP: PointDiff = PointDiff { y: -1, x: 0 };
+const DOWN: PointDiff = PointDiff { y: 1, x: 0 };
+
+const CONNECT_LEFT: [u8; 3] = [b'F', b'L', b'-'];
+const CONNECT_RIGHT: [u8; 3] = [b'J', b'7', b'-'];
+const CONNECT_UP: [u8; 3] = [b'F', b'7', b'|'];
+const CONNECT_DOWN: [u8; 3] = [b'J', b'L', b'|'];
+
+fn replace_start(data: &mut Array2<u8>, start: &Point) {
+    let cons = [UP, DOWN, LEFT, RIGHT]
+        .iter()
+        .map(|diff| start.apply_diff(diff))
+        .map(|pt| pt.and_then(|p| data.get((p.y, p.x))))
+        .zip([CONNECT_UP, CONNECT_DOWN, CONNECT_LEFT, CONNECT_RIGHT])
+        .map(|(pt, connections)| {
+            if let Some(pt) = pt {
+                connections.contains(pt)
+            } else {
+                false
+            }
+        })
+        .collect::<Vec<_>>();
+
+    let new = match (cons[0], cons[1], cons[2], cons[3]) {
+        (true, true, false, false) => b'|',
+        (true, false, true, false) => b'J',
+        (true, false, false, true) => b'L',
+        (false, false, true, true) => b'-',
+        (false, true, true, false) => b'7',
+        (false, true, false, true) => b'F',
+        _ => panic!("can't find connections for start"),
+    };
+
+    data[(start.y, start.x)] = new;
+}
+
+fn get_main_loop(data: &mut Array2<u8>) -> AHashSet<Point> {
     let start_location = data
         .indexed_iter()
         .filter(|(_, &d)| d == b'S')
         .map(|(idx, _)| idx)
         .next()
         .expect("can't find start");
-        
+
     let mut visited = AHashSet::new();
-    
-    let mut next = Point { y: start_location.0, x: start_location.1 };
-    
+
+    let mut next = Point {
+        y: start_location.0,
+        x: start_location.1,
+    };
+
+    replace_start(data, &next);
+
     loop {
-        visited.insert(next.clone());
+        visited.insert(next);
         let n = get_rules_for(data[(next.y, next.x)])
             .into_iter()
             .filter_map(|diff| next.apply_diff(&diff))
-            .filter(|p| !visited.contains(p))
-            .next();
-            
+            .find(|p| !visited.contains(p));
+
         if let Some(n) = n {
             next = n;
         } else {
             break;
         }
     }
-    
+
     visited
 }
 
-fn calculate_inside(data: &Array2<u8>, main_loop: &AHashSet<Point>) -> usize {
+fn calculate_inside(data: Array2<u8>, main_loop: &AHashSet<Point>) -> usize {
     let mut result = 0;
     for y in 0..data.dim().0 {
         let mut inside = false;
-        
+
         for x in 0..data.dim().1 {
             let cell = data[(y, x)];
-            
+
             let in_main_loop = main_loop.contains(&Point { y, x });
-            
-            // TODO: handle "S" properly
+
             if in_main_loop && (cell == b'|' || cell == b'J' || cell == b'L') {
                 inside = !inside
             }
-            
+
             if !in_main_loop && inside {
                 result += 1;
             }
@@ -136,13 +145,13 @@ fn calculate_inside(data: &Array2<u8>, main_loop: &AHashSet<Point>) -> usize {
     result
 }
 
-fn calculate(data: &Array2<u8>) -> (usize, usize) {
-    let main_loop = get_main_loop(data);
-    
+fn calculate(mut data: Array2<u8>) -> (usize, usize) {
+    let main_loop = get_main_loop(&mut data);
+
     let p1 = main_loop.len() / 2;
-    
+
     let p2 = calculate_inside(data, &main_loop);
-    
+
     (p1, p2)
 }
 
@@ -152,7 +161,7 @@ fn main() {
     let inp = fs::read_to_string(args.input).expect("can't open input file");
 
     let data = parse(&inp);
-    let (p1, p2) = calculate(&data);
+    let (p1, p2) = calculate(data);
     println!("{}\n{}", p1, p2);
 }
 
@@ -168,32 +177,32 @@ mod tests {
 
     #[test]
     fn test_p1_example() {
-        assert_eq!(calculate(&parse(EXAMPLE_DATA_P1)).0, 8);
+        assert_eq!(calculate(parse(EXAMPLE_DATA_P1)).0, 8);
     }
 
     #[test]
     fn test_p2_example_1() {
-        assert_eq!(calculate(&parse(EXAMPLE_DATA_P2_1)).1, 4);
+        assert_eq!(calculate(parse(EXAMPLE_DATA_P2_1)).1, 4);
     }
 
     #[test]
     fn test_p2_example_2() {
-        assert_eq!(calculate(&parse(EXAMPLE_DATA_P2_2)).1, 8);
+        assert_eq!(calculate(parse(EXAMPLE_DATA_P2_2)).1, 8);
     }
 
     #[test]
     fn test_p2_example_3() {
-        assert_eq!(calculate(&parse(EXAMPLE_DATA_P2_3)).1, 10);
+        assert_eq!(calculate(parse(EXAMPLE_DATA_P2_3)).1, 10);
     }
 
     #[test]
     fn test_p1_real() {
-        assert_eq!(calculate(&parse(REAL_DATA)).0, 6831);
+        assert_eq!(calculate(parse(REAL_DATA)).0, 6831);
     }
 
     #[test]
     fn test_p2_real() {
-        assert_eq!(calculate(&parse(REAL_DATA)).1, 305);
+        assert_eq!(calculate(parse(REAL_DATA)).1, 305);
     }
 
     #[cfg(feature = "bench")]
@@ -209,9 +218,8 @@ mod tests {
         }
 
         #[bench]
-        fn bench_calculate(b: &mut Bencher) {
-            let parsed = parse(black_box(REAL_DATA));
-            b.iter(|| calculate(black_box(&parsed)));
+        fn bench_parse_and_calculate(b: &mut Bencher) {
+            b.iter(|| calculate(parse(black_box(REAL_DATA))));
         }
     }
 }

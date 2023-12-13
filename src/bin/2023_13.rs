8ae7b5a4da4e2085817cd5aa05a1f99a184d22c4
Tom Willemsen <tomwillemsen1995@gmail.com>
2023-12-13 08:09:16 +0000

    13: don't bruteforce

--- src/bin/2023_13.rs
+++ src/bin/2023_13.rs
@@ -1,7 +1,6 @@
 #![cfg_attr(feature = "bench", feature(test))]
 use advent_of_code_2023::{Cli, Parser};
 use ndarray::{s, Array2};
-use rayon::prelude::*;
 use std::fs;
 
 fn make_grid(raw_inp: &str) -> Array2<bool> {
@@ -26,71 +25,55 @@ fn parse(raw_inp: &str) -> Vec<Array2<bool>> {
     raw_inp.trim().split("\n\n").map(make_grid).collect()
 }
 
-fn get_y_reflection(arr: &Array2<bool>, disallowed: Option<usize>) -> Option<usize> {
+fn get_y_reflection<const WANTED_DIFF: usize>(arr: &Array2<bool>) -> Option<usize> {
     (0..arr.dim().0 - 1)
-        .filter(|start_y| Some(start_y + 1) != disallowed)
-        .filter(|start_y| {
-            (0..=*start_y)
+        .filter(|&start_y| {
+            (0..=start_y)
                 .rev()
                 .zip(start_y + 1..arr.dim().0)
-                .all(|(r1, r2)| arr.slice(s![r1, ..]) == arr.slice(s![r2, ..]))
+                .map(|(r1, r2)| {
+                    arr.slice(s![r1, ..]).iter()
+                        .zip(arr.slice(s![r2, ..]))
+                        .map(|(e1, e2)| if e1 != e2 { 1 } else { 0 })
+                        .sum::<usize>()
+                })
+                .sum::<usize>() == WANTED_DIFF
         })
         .map(|start_y| start_y + 1)
         .next()
 }
 
-fn get_x_reflection(arr: &Array2<bool>, disallowed: Option<usize>) -> Option<usize> {
+fn get_x_reflection<const WANTED_DIFF: usize>(arr: &Array2<bool>) -> Option<usize> {
     (0..arr.dim().1 - 1)
-        .filter(|start_x| Some(start_x + 1) != disallowed)
-        .filter(|start_x| {
-            (0..=*start_x)
+        .filter(|&start_x| {
+            (0..=start_x)
                 .rev()
                 .zip(start_x + 1..arr.dim().1)
-                .all(|(c1, c2)| arr.slice(s![.., c1]) == arr.slice(s![.., c2]))
+                .map(|(c1, c2)| {
+                    arr.slice(s![.., c1]).iter()
+                        .zip(arr.slice(s![.., c2]))
+                        .map(|(e1, e2)| if e1 != e2 { 1 } else { 0 })
+                        .sum::<usize>()
+                    
+                })
+                .sum::<usize>() == WANTED_DIFF
         })
         .map(|start_x| start_x + 1)
         .next()
 }
 
-fn get_p2(arr: &mut Array2<bool>, p1_y: Option<usize>, p1_x: Option<usize>) -> usize {
-    for y in 0..arr.dim().0 {
-        for x in 0..arr.dim().1 {
-            arr[(y, x)] = !arr[(y, x)];
-
-            let y_refl = get_y_reflection(arr, p1_y);
-            let x_refl = get_x_reflection(arr, p1_x);
-
-            arr[(y, x)] = !arr[(y, x)];
-
-            if let Some(res) = y_refl {
-                return 100 * res;
-            }
-
-            if let Some(res) = x_refl {
-                return res;
-            }
-        }
-    }
-    panic!("can't locate smudge");
-}
-
-fn calculate(data: &mut [Array2<bool>]) -> (usize, usize) {
-    data.par_iter_mut()
+fn calculate<const WANTED_DIFF: usize>(data: &[Array2<bool>]) -> usize {
+    data.iter()
         .map(|arr| {
-            let p1_y_refl = get_y_reflection(arr, None);
-            let p1_x_refl = get_x_reflection(arr, None);
-
-            let p1 = if let Some(res) = p1_y_refl {
+            if let Some(res) = get_y_reflection::<WANTED_DIFF>(arr) {
                 res * 100
-            } else if let Some(res) = p1_x_refl {
+            } else if let Some(res) = get_x_reflection::<WANTED_DIFF>(arr) {
                 res
             } else {
-                panic!("no p1 answer");
-            };
-
-            (p1, get_p2(arr, p1_y_refl, p1_x_refl))
+                panic!("no answer");
+            }
         })
-        .reduce(|| (0, 0), |acc, e| (acc.0 + e.0, acc.1 + e.1))
+        .sum()
 }
 
 fn main() {
@@ -98,8 +81,9 @@ fn main() {
 
     let inp = fs::read_to_string(args.input).expect("can't open input file");
 
-    let mut data = parse(&inp);
-    let (p1, p2) = calculate(&mut data);
+    let data = parse(&inp);
+    let p1 = calculate::<0>(&data);
+    let p2 = calculate::<1>(&data);
     println!("{}\n{}", p1, p2);
 }
 
@@ -112,22 +96,22 @@ mod tests {
 
     #[test]
     fn test_p1_example() {
-        assert_eq!(calculate(&mut parse(EXAMPLE_DATA)).0, 405);
+        assert_eq!(calculate::<0>(&parse(EXAMPLE_DATA)), 405);
     }
 
     #[test]
     fn test_p2_example() {
-        assert_eq!(calculate(&mut parse(EXAMPLE_DATA)).1, 400);
+        assert_eq!(calculate::<1>(&parse(EXAMPLE_DATA)), 400);
     }
 
     #[test]
     fn test_p1_real() {
-        assert_eq!(calculate(&mut parse(REAL_DATA)).0, 32723);
+        assert_eq!(calculate::<0>(&parse(REAL_DATA)), 32723);
     }
 
     #[test]
     fn test_p2_real() {
-        assert_eq!(calculate(&mut parse(REAL_DATA)).1, 34536);
+        assert_eq!(calculate::<1>(&parse(REAL_DATA)), 34536);
     }
 
     #[cfg(feature = "bench")]
